2954f626e793ef44beab45b6bd6be4a1cf73da6e
Reiji <reiji.210@icloud.com>
2022-08-05 04:40:32 +0900

    edit matlab files

--- matlab/slalom/slalom_plot.m
+++ matlab/slalom/slalom_plot.m
@@ -14,7 +14,7 @@ ref = csvread("ref.csv");
 ref_x = ref(:,1);
 ref_y = ref(:,2);
 ref_theta = ref(:,3);
-ref_v = 0.5481587246 * ones(length(data_x),1);
+ref_v = 0.5064989 * ones(length(data_x),1);
 ref_omega = ref(:,4);
 %% plot
 figure(1);
@@ -25,12 +25,12 @@ grid on;
 hold on;
 plot(ref_x,ref_y,'LineWidth',2);
 pbaspect([1 1 1]);
-xlabel('$x$ [m]','Interpreter','latex','FontSize',15);
-ylabel('$y$ [m]','Interpreter','latex','FontSize',15);
+xlabel('$x$ [m]','Interpreter','latex','FontSize',25);
+ylabel('$y$ [m]','Interpreter','latex','FontSize',25);
 legend('data','ref','Interpreter','latex','Location','northwest','FontSize',15)
 h_axes = gca;
-h_axes.XAxis.FontSize = 15;
-h_axes.YAxis.FontSize = 15;
+h_axes.XAxis.FontSize = 25;
+h_axes.YAxis.FontSize = 25;
 
 subplot(3,2,2);
 plot(data_v,'LineWidth',2);
@@ -38,26 +38,26 @@ grid on;
 hold on;
 plot(ref_v,'LineWidth',2);
 plot(data_kanayama_v,'LineWidth',2);
-xlabel('Time [ms]','Interpreter','latex','FontSize',15);
-ylabel('$v$ [m/s]','Interpreter','latex','FontSize',15);
+xlabel('Time [ms]','Interpreter','latex','FontSize',25);
+ylabel('$v$ [m/s]','Interpreter','latex','FontSize',25);
 xlim([0 length(data_v)])
-legend('data','ref','kanayama','Interpreter','latex','Location','northwest','FontSize',15)
+legend('data','ref','kanayama','Interpreter','latex','Location','northwest','FontSize',20)
 h_axes = gca;
-h_axes.XAxis.FontSize = 15;
-h_axes.YAxis.FontSize = 15;
+h_axes.XAxis.FontSize = 25;
+h_axes.YAxis.FontSize = 25;
 
 subplot(3,2,4);
 plot(data_theta,'LineWidth',2);
 grid on;
 hold on;
 plot(ref_theta,'LineWidth',2);
-xlabel('Time [ms]','Interpreter','latex','FontSize',15);
-ylabel('$\theta$ [rad]','Interpreter','latex','FontSize',15);
+xlabel('Time [ms]','Interpreter','latex','FontSize',25);
+ylabel('$\theta$ [rad]','Interpreter','latex','FontSize',25);
 xlim([0 length(data_theta)])
-legend('data','ref','Interpreter','latex','Location','northwest','FontSize',15)
+legend('data','ref','Interpreter','latex','Location','northwest','FontSize',20)
 h_axes = gca;
-h_axes.XAxis.FontSize = 15;
-h_axes.YAxis.FontSize = 15;
+h_axes.XAxis.FontSize = 25;
+h_axes.YAxis.FontSize = 25;
 
 subplot(3,2,6);
 plot(data_omega,'LineWidth',2);
@@ -65,10 +65,10 @@ grid on;
 hold on;
 plot(ref_omega,'LineWidth',2);
 plot(data_kanayama_w,'LineWidth',2);
-xlabel('Time [ms]','Interpreter','latex','FontSize',15);
-ylabel('$\omega$ [rad/s]','Interpreter','latex','FontSize',15);
+xlabel('Time [ms]','Interpreter','latex','FontSize',25);
+ylabel('$\omega$ [rad/s]','Interpreter','latex','FontSize',25);
 xlim([0 length(data_theta)])
-legend('data','ref','kanayama','Interpreter','latex','Location','northwest','FontSize',15)
+legend('data','ref','kanayama','Interpreter','latex','Location','northwest','FontSize',20)
 h_axes = gca;
-h_axes.XAxis.FontSize = 15;
-h_axes.YAxis.FontSize = 15;
+h_axes.XAxis.FontSize = 25;
+h_axes.YAxis.FontSize = 25;
