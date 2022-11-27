22f95bbe4fcbf9cd1b18c27e834dcf5d7c1fc769
Reiji <reiji.210@icloud.com>
2022-11-27 14:30:06 +0900

    add ref and log data

--- matlab/acceleration/accelerarion.m
+++ matlab/acceleration/accelerarion.m
@@ -1,4 +1,4 @@
-%% length = 0.144
+%% length = 0.144 - 0.006 = 0.138
 clear
 close all
 ts = 0.001;
@@ -6,26 +6,26 @@ ts = 0.001;
 v_max = 0.3;
 a = v_max / 0.3;
 
-t = 0:ts:0.78;
+t = 0:ts:0.76;
 
 for i = 1:300
     v_ref(i) = a * t(i);
 end
 
-for i = 301:480
+for i = 301:460
     v_ref(i) = v_max;
 end
 
-for i = 481:780
-    v_ref(i) = v_max - a * (t(i) - 0.48);
+for i = 461:760
+    v_ref(i) = v_max - a * (t(i) - 0.46);
 end
-v_ref(781) = 0;
+v_ref(761) = 0;
 
 v_ref = v_ref';
 plot(t, v_ref, 'LineWidth',3);
 
 length = 0;
-for i = 1:781
+for i = 1:761
     length = length + v_ref(i) * ts;
 end
 
@@ -96,3 +96,35 @@ for i = 1:651
 end
 
 dlmwrite('ref3.csv', v_ref, 'precision', '%.3f');
+%% length = 0.9 - 0.36 = 0.54
+clear
+close all
+ts = 0.001;
+
+v_max = 0.2;
+a = v_max / 0.2;
+
+t = 0:ts:0.47;
+
+for i = 1:200
+    v_ref(i) = a * t(i);
+end
+
+for i = 201:270
+    v_ref(i) = v_max;
+end
+
+for i = 271:470
+    v_ref(i) = v_max - a * (t(i) - 0.27);
+end
+v_ref(471) = 0;
+
+v_ref = v_ref';
+plot(t, v_ref, 'LineWidth',3);
+
+length = 0;
+for i = 1:471
+    length = length + v_ref(i) * ts;
+end
+
+dlmwrite('ref4.csv', v_ref, 'precision', '%.3f');
