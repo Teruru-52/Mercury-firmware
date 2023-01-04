eb23d276cd4eb742415c594687aae0fd8f15ab09
Reiji <reiji.210@icloud.com>
2023-01-04 14:24:51 +0900

    change reference directory

--- Core/Inc/my_header.h
+++ Core/Inc/my_header.h
@@ -1,15 +1,15 @@
 #ifndef _MY_HEADER_H_
 #define _MY_HEADER_H_
 
-#include "../../Application/Inc/hardware/led.h"
-#include "../../Application/Inc/hardware/ir_sensor.h"
-#include "../../Application/Inc/hardware/speaker.h"
-#include "../../Application/Inc/controller/controller.h"
-#include "../../Application/Inc/odometory.h"
-#include "../../Application/Inc/digital_filter.h"
-#include "../../Application/Inc/controller/m_identification.h"
-#include "../../Application/Inc/controller/step_identification.h"
-#include "../../Application/Inc/state.h"
+#include "hardware/led.h"
+#include "hardware/ir_sensor.h"
+#include "hardware/speaker.h"
+#include "controller/controller.h"
+#include "odometory.h"
+#include "digital_filter.h"
+#include "controller/m_identification.h"
+#include "controller/step_identification.h"
+#include "state.h"
 
 #include "MazeSolver_conf.h"
 #include "Maze.h"
