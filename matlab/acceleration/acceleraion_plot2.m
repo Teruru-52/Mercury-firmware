510a4bb63285d4fb3518f50d1d8d1eeb153b9d08
Reiji <reiji.210@icloud.com>
2023-01-05 00:25:00 +0900

    add acc data

--- matlab/acceleration/acceleraion_plot2.m
+++ matlab/acceleration/acceleraion_plot2.m
@@ -7,9 +7,10 @@ data = csvread("data_138.csv");
 
 data_l = data(:,1);
 data_v = data(:,2);
-ref_l = data(:,3);
-ref_v = data(:,4);
-ref_a = data(:,5);
+data_a = data(:,3);
+ref_l = data(:,4);
+ref_v = data(:,5);
+ref_a = data(:,6);
 
 n = length(ref_v);
 t = 0:1:n-1;
@@ -48,10 +49,12 @@ h_axes.YAxis.FontSize = 25;
 subplot(3,1,3);
 plot(t, ref_a,'LineWidth',3);
 grid on;
+hold on;
+plot(t, data_a,'LineWidth',3);
 xlabel('Time [s]','Interpreter','latex','FontSize',25);
 ylabel('$a$ [m/s$^2$]','Interpreter','latex','FontSize',25);
 xlim([0 n*ts])
-legend('ref','Interpreter','latex','Location','northwest','FontSize',25)
+legend('ref','data','Interpreter','latex','Location','northwest','FontSize',25)
 h_axes = gca;
 h_axes.XAxis.FontSize = 25;
 h_axes.YAxis.FontSize = 25;
