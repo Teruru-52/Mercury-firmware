a8ef3fd63eff5a656d649a30c7afd64abf2838f5
Reiji <reiji.210@icloud.com>
2023-01-04 21:36:34 +0900

    edit data

--- matlab/acceleration/acceleraion_plot2.m
+++ matlab/acceleration/acceleraion_plot2.m
@@ -9,6 +9,7 @@ data_l = data(:,1);
 data_v = data(:,2);
 ref_l = data(:,3);
 ref_v = data(:,4);
+ref_a = data(:,5);
 
 n = length(ref_v);
 t = 0:1:n-1;
@@ -18,7 +19,7 @@ t = t';
 f1 = figure(1);  
 set(f1, 'position', get(0, 'screensize'))
 
-subplot(2,1,1);
+subplot(3,1,1);
 plot(t, ref_l,'LineWidth',3);
 grid on;
 hold on;
@@ -31,7 +32,7 @@ h_axes = gca;
 h_axes.XAxis.FontSize = 25;
 h_axes.YAxis.FontSize = 25;
 
-subplot(2,1,2);
+subplot(3,1,2);
 plot(t, ref_v,'LineWidth',3);
 grid on;
 hold on;
@@ -40,6 +41,17 @@ xlabel('Time [s]','Interpreter','latex','FontSize',25);
 ylabel('$v$ [m/s]','Interpreter','latex','FontSize',25);
 xlim([0 n*ts])
 legend('ref','data','Interpreter','latex','Location','northwest','FontSize',25)
+h_axes = gca;
+h_axes.XAxis.FontSize = 25;
+h_axes.YAxis.FontSize = 25;
+
+subplot(3,1,3);
+plot(t, ref_a,'LineWidth',3);
+grid on;
+xlabel('Time [s]','Interpreter','latex','FontSize',25);
+ylabel('$a$ [m/s$^2$]','Interpreter','latex','FontSize',25);
+xlim([0 n*ts])
+legend('ref','Interpreter','latex','Location','northwest','FontSize',25)
 h_axes = gca;
 h_axes.XAxis.FontSize = 25;
 h_axes.YAxis.FontSize = 25;
