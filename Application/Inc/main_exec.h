392b64a4332007c1950bbcc7835442ff21523da3
Reiji <reiji.210@icloud.com>
2023-02-19 15:27:30 +0900

    add main_exec files

---
+++ Application/Inc/main_exec.h
@@ -0,0 +1,30 @@
+/*
+ * main_exec.h
+ *
+ *  Created on: Febrary 13th, 2023
+ *      Author: Reiji Terunuma
+ */
+
+#ifndef MAIN_EXEC_H_
+#define MAIN_EXEC_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include "main.h"
+
+    void StartupProcess();
+    void SelectFunc(int16_t pulse);
+    void Initialize();
+    void UpdateUndercarriage();
+    void Notification();
+    void MazeSearch();
+    // void TimeAttack();
+    void StateProcess();
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif // MAIN_EXEC_H_
