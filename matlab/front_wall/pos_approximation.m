19d3b8bd7f2e430f5ed9efde36d679d09d0878f6
Reiji <reiji.210@icloud.com>
2024-01-31 12:28:37 +0900

    refactor: MATLAB files for plot

--- matlab/front_wall/pos_approximation.m
+++ matlab/front_wall/pos_approximation.m
@@ -2,7 +2,8 @@ clear;
 close all;
 
 %% load log
-data = csvread("data.csv");
+% data = csvread("data_long_x.csv");
+data = csvread("data_short_x.csv");
 n = length(data);
 
 ir_value = data(1:2:n, :); % odd rows
@@ -10,7 +11,7 @@ pos = data(2:2:n, :); % even rows
 
 ir_sl = ir_value(:,3);
 ir_sr = ir_value(:,4);
-ir_s = (ir_sl + ir_sr) / 2; 
+ir_s = (ir_sl + ir_sr) / 2;
 
 pos_x = pos(:,1);
 % pos_y = pos(:,2);
@@ -19,14 +20,34 @@ n = length(pos_x);
 ts = 0.2;
 t = 0:1:n-1;
 t = t'*ts;
+
+%% curve fitting (logarithmic model)
+%     log_fittype = fittype("a*log(b*x+c)+d", dependent="y", independent="x", coefficients=["a" "b" "c" "d"]);
+    log_fittype = fittype("a*log(max(1e-15, b * x + c))+d", dependent="y", independent="x", coefficients=["a" "b" "c" "d"]);
+
+    a = 20; b = 0.005;  c = 1 - 2287*b; d = 0.01;
+    % b*ir_s + c % must be positive
+    initialCoefficients = [a, b, c, d];
+    for i = 1:10
+        if i > 1
+            initialCoefficients = [f.a, f.b, f.c, f.d];
+        end
+        f = fit(ir_s, pos_x, log_fittype, 'StartPoint', initialCoefficients)
+    end
+    pos_x_fit = f.a.*log(f.b*ir_s + f.c) + f.d;
+    % pos_x_fit = a.*log(b*ir_s + c) + d;
 %% plot data
 close all;
-f1 = figure(1);  
+f1 = figure(1);
 set(f1, 'position', get(0, 'screensize'))
+ir_max = 2800;
+ir_min = 2000;
 
 subplot(3,2,[1,3]);
-plot(ir_s, pos_x,'LineWidth',3);
+plot(ir_s, pos_x, 'o', 'MarkerSize',6);
 grid on;
+hold on;
+plot(ir_s, pos_x_fit,'LineWidth',3);
 xlabel('IR sensor value (mean)','Interpreter','latex');
 ylabel('$x$ [mm]','Interpreter','latex');
 set(gca, "FontName", "Times New Roman", "FontSize", 15);
@@ -45,6 +66,7 @@ grid on;
 xlabel('Time [s]','Interpreter','latex');
 ylabel('IR sensor value (mean)','Interpreter','latex');
 xlim([0 max(t)])
+ylim([ir_min ir_max]);
 set(gca, "FontName", "Times New Roman", "FontSize", 15);
 
 subplot(3,2,5);
@@ -53,6 +75,7 @@ grid on;
 xlabel('Time [s]','Interpreter','latex');
 ylabel('IR sensor value (left)','Interpreter','latex');
 xlim([0 max(t)])
+ylim([ir_min ir_max]);
 set(gca, "FontName", "Times New Roman", "FontSize", 15);
 
 subplot(3,2,6);
@@ -61,4 +84,5 @@ grid on;
 xlabel('Time [s]','Interpreter','latex');
 ylabel('IR sensor value (right)','Interpreter','latex');
 xlim([0 max(t)])
+ylim([ir_min ir_max]);
 set(gca, "FontName", "Times New Roman", "FontSize", 15);
