8b95d6976002b1bf1d2bd632c08cdee29c3ca5c7
Reiji <reiji.210@icloud.com>
2024-02-06 23:45:42 +0900

    chore: remove docs/html from git management

--- docs/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['quatro_5fcpp_5fubuntu_0',['Quatro_cpp_ubuntu',['../md_README.html',1,'']]]
-];
--- docs/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['quatro_5fcpp_5fubuntu_1',['Quatro_cpp_ubuntu',['../md_README.html',1,'']]]
-];
